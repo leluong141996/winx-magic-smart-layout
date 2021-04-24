aa14acfaff945d2810f5867b04489551b6229830
nguyenquythuc <nguyenquythuc@gapo.com.vn>
2021-04-24 11:51:24 +0700

    update

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    return render_template('result.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000, debug=True)
